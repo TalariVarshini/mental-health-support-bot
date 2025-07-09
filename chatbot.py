6ed957d870b9e53ea34dde0a7d85df3bb83b036e
Varshini Talari <varshinitalari525@gmail.com>
2025-07-09 21:52:03 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,16 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+chat_history_ids = None
+
+def get_chat_response(user_input, chat_history=None):
+    global chat_history_ids
+    new_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
+    bot_input_ids = torch.cat([chat_history_ids, new_input_ids], dim=-1) if chat_history else new_input_ids
+
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    return response
